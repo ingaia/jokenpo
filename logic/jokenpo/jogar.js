40df557ed35a8960c8329faf37ae225e2f3ab7dd
Victor Perin <me@victorperin.ninja>
2017-02-26 09:35:38 -0300

    init(logic/jokenpo/jogar)

---
+++ logic/jokenpo/jogar.js
@@ -0,0 +1,16 @@
+module.exports = (p1, p2) => {
+  if(p1 === p2) return 0;
+
+  const possibleHands = ['pedra', 'tesoura', 'papel'];
+
+  const p1Index = possibleHands.indexOf(p1);
+  const p2Index = possibleHands.indexOf(p2);
+
+  let winner = 0;
+
+  if(p1Index === 0 && p2Index === 2) winner = 2;
+  else if( p1Index < p2Index || (p1Index === 2 && p2Index === 0) ) winner = 1;
+  else winner = 2;
+
+  return winner;
+}
