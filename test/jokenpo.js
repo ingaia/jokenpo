2f6f5ed7082583882f08381552c55e1e7b28afc7
Victor Perin <me@victorperin.ninja>
2017-03-02 00:25:38 -0300

    style(test/jokenpo): corrigindo espaçamentos

--- test/jokenpo.js
+++ test/jokenpo.js
@@ -1,22 +1,22 @@
 const assert = require('assert')
 
 describe('Jogar Jokenpo', () => {
-  const jogarJokenpo = require('../jogarJokenpo.js');
-  it('jogador 1 ganha', () => {
-    assert.equal(jogarJokenpo(['pedra', 'tesoura']), 1);
-    assert.equal(jogarJokenpo(['tesoura', 'papel']), 1);
-    assert.equal(jogarJokenpo(['papel', 'pedra']), 1);
-  });
+    const jogarJokenpo = require('../jogarJokenpo.js');
+    it('jogador 1 ganha', () => {
+        assert.equal(jogarJokenpo(['pedra', 'tesoura']), 1);
+        assert.equal(jogarJokenpo(['tesoura', 'papel']), 1);
+        assert.equal(jogarJokenpo(['papel', 'pedra']), 1);
+    });
 
-  it('jogador 2 ganha', () => {
-    assert.equal(jogarJokenpo(['tesoura', 'pedra']), 2);
-    assert.equal(jogarJokenpo(['papel', 'tesoura']), 2);
-    assert.equal(jogarJokenpo(['pedra', 'papel']), 2);
-  });
+    it('jogador 2 ganha', () => {
+        assert.equal(jogarJokenpo(['tesoura', 'pedra']), 2);
+        assert.equal(jogarJokenpo(['papel', 'tesoura']), 2);
+        assert.equal(jogarJokenpo(['pedra', 'papel']), 2);
+    });
 
-  it('empate', () => {
-    assert.equal(jogarJokenpo(['pedra', 'pedra']), 0);
-    assert.equal(jogarJokenpo(['tesoura', 'tesoura']), 0);
-    assert.equal(jogarJokenpo(['papel', 'papel']), 0);
-  });
+    it('empate', () => {
+        assert.equal(jogarJokenpo(['pedra', 'pedra']), 0);
+        assert.equal(jogarJokenpo(['tesoura', 'tesoura']), 0);
+        assert.equal(jogarJokenpo(['papel', 'papel']), 0);
+    });
 });
