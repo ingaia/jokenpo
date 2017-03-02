fbf1fe9d7143f385ffd19a2c18d943b09b2a4baf
Victor Perin <me@victorperin.ninja>
2017-03-02 00:11:54 -0300

    init(respostas-resultados)

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 Promise
     .resolve(['pedra', 'papel'])
     .then(require('./jogarJokenpo'))
+    .then(require('./respostasDeResultados'))
     .then(console.log)
---
+++ respostasDeResultados.js
@@ -0,0 +1,1 @@
+module.exports = (resultado) => ['Empate', 'Jogador 1 ganhou!', 'Jogador 2 ganhou!'][resultado]
